45ff4f12eade80fd779dd9bb78739d9429ba17d3
민지 김 <rlaalswl5320@naver.com>
2025-10-17 14:31:22 +0900

    김민지＿도서　등록창과　도서　대여　및　반납　창

--- library_support_system/Views/Book_Rental.Designer.cs
+++ library_support_system/Views/Book_Rental.Designer.cs
@@ -28,22 +28,146 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
+            this.cmbGen = new System.Windows.Forms.ComboBox();
+            this.txtBthDate = new System.Windows.Forms.TextBox();
+            this.oracleDataAdapter1 = new Oracle.ManagedDataAccess.Client.OracleDataAdapter();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.oracleCommandBuilder1 = new Oracle.ManagedDataAccess.Client.OracleCommandBuilder();
+            this.Book_Picture = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn9 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn10 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn11 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnRent = new System.Windows.Forms.DataGridViewButtonColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
-            // book_rental
+            // button1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.button1.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button1.Location = new System.Drawing.Point(753, 42);
+            this.button1.Margin = new System.Windows.Forms.Padding(6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(186, 53);
+            this.button1.TabIndex = 30;
+            this.button1.Text = "조회";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // cmbGen
+            // 
+            this.cmbGen.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbGen.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.cmbGen.FormattingEnabled = true;
+            this.cmbGen.Items.AddRange(new object[] {
+            "제목",
+            "저자"});
+            this.cmbGen.Location = new System.Drawing.Point(26, 42);
+            this.cmbGen.Margin = new System.Windows.Forms.Padding(6);
+            this.cmbGen.Name = "cmbGen";
+            this.cmbGen.Size = new System.Drawing.Size(186, 53);
+            this.cmbGen.TabIndex = 29;
+            // 
+            // txtBthDate
+            // 
+            this.txtBthDate.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.txtBthDate.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtBthDate.Font = new System.Drawing.Font("맑은 고딕", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.txtBthDate.Location = new System.Drawing.Point(224, 42);
+            this.txtBthDate.Margin = new System.Windows.Forms.Padding(6);
+            this.txtBthDate.Multiline = true;
+            this.txtBthDate.Name = "txtBthDate";
+            this.txtBthDate.Size = new System.Drawing.Size(517, 53);
+            this.txtBthDate.TabIndex = 28;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Book_Picture,
+            this.dataGridViewTextBoxColumn9,
+            this.dataGridViewTextBoxColumn10,
+            this.dataGridViewTextBoxColumn11,
+            this.btnRent});
+            this.dataGridView1.Location = new System.Drawing.Point(26, 113);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 82;
+            this.dataGridView1.RowTemplate.Height = 37;
+            this.dataGridView1.Size = new System.Drawing.Size(1272, 260);
+            this.dataGridView1.TabIndex = 46;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Book_Picture
+            // 
+            this.Book_Picture.HeaderText = "도서 이미지";
+            this.Book_Picture.MinimumWidth = 10;
+            this.Book_Picture.Name = "Book_Picture";
+            this.Book_Picture.Width = 200;
+            // 
+            // dataGridViewTextBoxColumn9
+            // 
+            this.dataGridViewTextBoxColumn9.HeaderText = "책 제목";
+            this.dataGridViewTextBoxColumn9.MinimumWidth = 10;
+            this.dataGridViewTextBoxColumn9.Name = "dataGridViewTextBoxColumn9";
+            this.dataGridViewTextBoxColumn9.Width = 200;
+            // 
+            // dataGridViewTextBoxColumn10
+            // 
+            this.dataGridViewTextBoxColumn10.HeaderText = "저자";
+            this.dataGridViewTextBoxColumn10.MinimumWidth = 10;
+            this.dataGridViewTextBoxColumn10.Name = "dataGridViewTextBoxColumn10";
+            this.dataGridViewTextBoxColumn10.Width = 200;
+            // 
+            // dataGridViewTextBoxColumn11
+            // 
+            this.dataGridViewTextBoxColumn11.HeaderText = "출판사";
+            this.dataGridViewTextBoxColumn11.MinimumWidth = 10;
+            this.dataGridViewTextBoxColumn11.Name = "dataGridViewTextBoxColumn11";
+            this.dataGridViewTextBoxColumn11.Width = 200;
+            // 
+            // btnRent
+            // 
+            this.btnRent.HeaderText = "대여 여부";
+            this.btnRent.MinimumWidth = 10;
+            this.btnRent.Name = "btnRent";
+            this.btnRent.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.btnRent.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            this.btnRent.Text = "";
+            this.btnRent.Width = 200;
+            // 
+            // Book_Rental
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1256, 737);
+            this.ClientSize = new System.Drawing.Size(2041, 1179);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.cmbGen);
+            this.Controls.Add(this.txtBthDate);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.Name = "book_rental";
+            this.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.Name = "Book_Rental";
             this.Text = "book_rental";
             this.Load += new System.EventHandler(this.book_rental_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox cmbGen;
+        private System.Windows.Forms.TextBox txtBthDate;
+        private Oracle.ManagedDataAccess.Client.OracleDataAdapter oracleDataAdapter1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private Oracle.ManagedDataAccess.Client.OracleCommandBuilder oracleCommandBuilder1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Book_Picture;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn9;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn10;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn11;
+        private System.Windows.Forms.DataGridViewButtonColumn btnRent;
     }
 }
--- library_support_system/Views/Book_Return.Designer.cs
+++ library_support_system/Views/Book_Return.Designer.cs
@@ -28,21 +28,231 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.cancel_button = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.txtWithDR = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.txtBth = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.txtGender = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.txtPhonenum = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.txtEmail = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.txtPicture = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.반납여부 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.txtBthDate = new System.Windows.Forms.TextBox();
+            this.cmbGen = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
-            // book_return
+            // dataGridView1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.txtWithDR,
+            this.name,
+            this.txtBth,
+            this.txtGender,
+            this.txtPhonenum,
+            this.txtEmail,
+            this.txtPicture,
+            this.반납여부,
+            this.Column1});
+            this.dataGridView1.Location = new System.Drawing.Point(89, 112);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(6, 4, 6, 4);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 27;
+            this.dataGridView1.Size = new System.Drawing.Size(1937, 914);
+            this.dataGridView1.TabIndex = 22;
+            // 
+            // cancel_button
+            // 
+            this.cancel_button.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.cancel_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cancel_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.cancel_button.Location = new System.Drawing.Point(1803, 1081);
+            this.cancel_button.Margin = new System.Windows.Forms.Padding(6);
+            this.cancel_button.Name = "cancel_button";
+            this.cancel_button.Size = new System.Drawing.Size(186, 60);
+            this.cancel_button.TabIndex = 24;
+            this.cancel_button.Text = "삭제";
+            this.cancel_button.UseVisualStyleBackColor = false;
+            // 
+            // btnSave
+            // 
+            this.btnSave.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.btnSave.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnSave.Location = new System.Drawing.Point(1606, 1081);
+            this.btnSave.Margin = new System.Windows.Forms.Padding(6);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(186, 60);
+            this.btnSave.TabIndex = 23;
+            this.btnSave.Text = "반납";
+            this.btnSave.UseVisualStyleBackColor = false;
+            // 
+            // txtWithDR
+            // 
+            this.txtWithDR.DataPropertyName = "Book_Title";
+            this.txtWithDR.HeaderText = "제목";
+            this.txtWithDR.MinimumWidth = 6;
+            this.txtWithDR.Name = "txtWithDR";
+            this.txtWithDR.ReadOnly = true;
+            this.txtWithDR.Width = 125;
+            // 
+            // name
+            // 
+            this.name.DataPropertyName = "Book_Explain";
+            this.name.HeaderText = "상세정보";
+            this.name.MinimumWidth = 6;
+            this.name.Name = "name";
+            this.name.ReadOnly = true;
+            this.name.Width = 125;
+            // 
+            // txtBth
+            // 
+            this.txtBth.DataPropertyName = "Book_Author";
+            this.txtBth.HeaderText = "저자";
+            this.txtBth.MinimumWidth = 6;
+            this.txtBth.Name = "txtBth";
+            this.txtBth.ReadOnly = true;
+            this.txtBth.Width = 125;
+            // 
+            // txtGender
+            // 
+            this.txtGender.DataPropertyName = "Book_Pbl";
+            this.txtGender.HeaderText = "출판사";
+            this.txtGender.MinimumWidth = 6;
+            this.txtGender.Name = "txtGender";
+            this.txtGender.ReadOnly = true;
+            this.txtGender.Width = 125;
+            // 
+            // txtPhonenum
+            // 
+            this.txtPhonenum.DataPropertyName = "User";
+            this.txtPhonenum.HeaderText = "회원정보";
+            this.txtPhonenum.MinimumWidth = 6;
+            this.txtPhonenum.Name = "txtPhonenum";
+            this.txtPhonenum.ReadOnly = true;
+            this.txtPhonenum.Width = 125;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.DataPropertyName = "Rental_Date";
+            this.txtEmail.HeaderText = "대여일";
+            this.txtEmail.MinimumWidth = 6;
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.ReadOnly = true;
+            this.txtEmail.Width = 125;
+            // 
+            // txtPicture
+            // 
+            this.txtPicture.DataPropertyName = "Rental_Return_Date";
+            this.txtPicture.HeaderText = "반납 예정일";
+            this.txtPicture.MinimumWidth = 6;
+            this.txtPicture.Name = "txtPicture";
+            this.txtPicture.ReadOnly = true;
+            this.txtPicture.Width = 125;
+            // 
+            // 반납여부
+            // 
+            this.반납여부.DataPropertyName = "Return";
+            this.반납여부.HeaderText = "반납여부";
+            this.반납여부.MinimumWidth = 10;
+            this.반납여부.Name = "반납여부";
+            this.반납여부.ReadOnly = true;
+            this.반납여부.Width = 200;
+            // 
+            // Column1
+            // 
+            this.Column1.DataPropertyName = "management";
+            this.Column1.HeaderText = "관리";
+            this.Column1.MinimumWidth = 10;
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            this.Column1.Width = 200;
+            // 
+            // txtBthDate
+            // 
+            this.txtBthDate.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.txtBthDate.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtBthDate.Font = new System.Drawing.Font("맑은 고딕", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.txtBthDate.Location = new System.Drawing.Point(287, 26);
+            this.txtBthDate.Margin = new System.Windows.Forms.Padding(6);
+            this.txtBthDate.Multiline = true;
+            this.txtBthDate.Name = "txtBthDate";
+            this.txtBthDate.Size = new System.Drawing.Size(517, 53);
+            this.txtBthDate.TabIndex = 25;
+            // 
+            // cmbGen
+            // 
+            this.cmbGen.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbGen.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.cmbGen.FormattingEnabled = true;
+            this.cmbGen.Items.AddRange(new object[] {
+            "제목",
+            "저자"});
+            this.cmbGen.Location = new System.Drawing.Point(89, 26);
+            this.cmbGen.Margin = new System.Windows.Forms.Padding(6);
+            this.cmbGen.Name = "cmbGen";
+            this.cmbGen.Size = new System.Drawing.Size(186, 53);
+            this.cmbGen.TabIndex = 26;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button1.Location = new System.Drawing.Point(816, 26);
+            this.button1.Margin = new System.Windows.Forms.Padding(6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(186, 53);
+            this.button1.TabIndex = 27;
+            this.button1.Text = "조회";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // Book_Return
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1256, 737);
+            this.ClientSize = new System.Drawing.Size(2041, 1179);
             this.ControlBox = false;
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.cmbGen);
+            this.Controls.Add(this.txtBthDate);
+            this.Controls.Add(this.cancel_button);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.dataGridView1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Name = "book_return";
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Name = "Book_Return";
             this.Text = "book_return";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button cancel_button;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.DataGridViewTextBoxColumn txtWithDR;
+        private System.Windows.Forms.DataGridViewTextBoxColumn name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn txtBth;
+        private System.Windows.Forms.DataGridViewTextBoxColumn txtGender;
+        private System.Windows.Forms.DataGridViewTextBoxColumn txtPhonenum;
+        private System.Windows.Forms.DataGridViewTextBoxColumn txtEmail;
+        private System.Windows.Forms.DataGridViewTextBoxColumn txtPicture;
+        private System.Windows.Forms.DataGridViewTextBoxColumn 반납여부;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.TextBox txtBthDate;
+        private System.Windows.Forms.ComboBox cmbGen;
+        private System.Windows.Forms.Button button1;
     }
 }
