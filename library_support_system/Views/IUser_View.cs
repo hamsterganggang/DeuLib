169c5b3c4063ff4fb1032f0bf07c0180538e2534
도완 김 <sulroc001932@naver.com>
2025-10-30 22:11:02 +0900

    회원조회 로직 추가

--- library_support_system/Views/IUser_View.cs
+++ library_support_system/Views/IUser_View.cs
@@ -1,9 +1,6 @@
 ﻿using library_support_system.Models;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace library_support_system.Views
@@ -11,24 +8,16 @@ namespace library_support_system.Views
     public interface IUser_View
     {
         #region Properties
-        string SearchValue { get; set; }
-        string SearchBy { get; set; }
+        string SearchValue { get; } // 검색어
+        string SearchBy { get; }     // 검색옵션(이름/전화번호)
         UserModel SelectedUser { get; }
-        bool IsRetireUserChecked { get; }
-        IEnumerable<UserModel> UserList { set; } // Presenter에서 View로 데이터를 전달하는 통로
+        IEnumerable<UserModel> UserList { set; }
         #endregion
 
         #region Events
-        //회원수정
         event EventHandler ChangeUserEvent;
-        //회원검색
         event EventHandler SearchEvent;
-        //회원삭제
         event EventHandler DeleteUserEvent;
-        //탈퇴회원조회
-        event EventHandler RetireFilterChanged;
-        //회원수정폼
-        void ShowUserEditForm(UserModel userToEdit);
         void ShowMessage(string message);
         #endregion
     }
